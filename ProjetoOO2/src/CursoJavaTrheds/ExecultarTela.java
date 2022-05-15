21f83ca20b6224697dcafdcec01e8236cbbe1430
DigitalMK <marcio@digitalmk.com.br>
2022-05-14 21:04:51 -0300

    javax.swing.JDialo 1º parte criação da tela

---
+++ ProjetoOO2/src/CursoJavaTrheds/ExecultarTela.java
@@ -0,0 +1,9 @@
+package CursoJavaTrheds;
+
+public class ExecultarTela {
+	
+	public static void main(String[] args) {
+		TelaTimeThread telaTimeThread = new TelaTimeThread();
+	}
+
+}
---
+++ ProjetoOO2/src/CursoJavaTrheds/TelaTimeThread.java
@@ -0,0 +1,25 @@
+package CursoJavaTrheds;
+
+import java.awt.Dimension;
+
+import javax.swing.JDialog;
+
+public class TelaTimeThread extends JDialog{
+	
+	public TelaTimeThread() {
+		
+		setTitle("Minha tela de time com thread.");
+		setSize(new Dimension(240, 240));
+		setLocationRelativeTo(null);
+		setResizable(false);
+		/* Primeira parte concluida*/
+		
+		
+		
+		setVisible(true);
+	}
+	
+	
+	
+
+}
