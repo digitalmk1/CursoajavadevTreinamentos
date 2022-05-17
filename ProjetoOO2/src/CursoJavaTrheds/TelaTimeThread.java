6f2434cfcb9b270fecebd45ad6910938fa6ec284
DigitalMK <marcio@digitalmk.com.br>
2022-05-17 15:23:49 -0300

    Trabalhando com THREADS

--- ProjetoOO2/src/CursoJavaTrheds/TelaTimeThread.java
+++ ProjetoOO2/src/CursoJavaTrheds/TelaTimeThread.java
@@ -6,12 +6,17 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.text.SimpleAttributeSet;
 
 public class TelaTimeThread extends JDialog{
 	
@@ -27,7 +32,47 @@ public class TelaTimeThread extends JDialog{
 	private JButton jButton = new JButton("Start");
 	private JButton jButton2 = new JButton(" Stop ");
 	
+   private Runnable thread1 = new Runnable() {
+	
+	@Override
+	public void run() {
+		
+		while(true) {
+	mostraTempo.setText(new SimpleDateFormat("dd/MM/yyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+	try {
+		Thread.sleep(1000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+					
+		}
+		
+	}
+};
 
+private Runnable thread2 = new Runnable() {
+	
+	@Override
+	public void run() {
+		
+		while(true) {
+	mostraTempo.setText(new SimpleDateFormat("dd/MM/yyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+	try {
+		Thread.sleep(1000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+					
+		}
+		
+	}
+};
+
+
+ private Thread thread1Time;
+ private Thread thread2Time;
 	
 	public TelaTimeThread() {
 		
@@ -72,6 +117,31 @@ public class TelaTimeThread extends JDialog{
 		 jButton.setPreferredSize(new Dimension(92,25));
 		 gridBagConstraints.gridx++;
 		 jPanel.add(jButton2, gridBagConstraints);
+		 
+		jButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			thread1Time = new Thread(thread1);
+			thread1Time.start();
+			
+			thread2Time = new Thread(thread2);
+			thread2Time.start();
+				
+				
+			}
+		});
+		
+		jButton2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				thread1Time.stop();
+				thread2Time.stop();
+				
+				
+			}
+		});
 		add(jPanel, BorderLayout.WEST);
 		setVisible(true);
 	}
