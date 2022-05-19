0ae7eb2cab99c68fffd7981ef3dba6a711c8e1f2
DigitalMK <marcio@digitalmk.com.br>
2022-05-19 20:32:22 -0300

    Fazendo leitura de arquivo TXT

--- ProjetoOO2/src/LerArquivoTXT/LerArqTXT.java
+++ ProjetoOO2/src/LerArquivoTXT/LerArqTXT.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class LerArquivoTXT {
+public class LerArqTXT {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
